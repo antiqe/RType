994254531913a60d807c039177b61953480a9287
antiqe <ronan.guiho@gmail.com>
2013-11-26 22:29:03 +0100

    patch state player missing

--- Network/Aggregator.cpp
+++ Network/Aggregator.cpp
@@ -14,79 +14,79 @@
 #include "Aggregator.hpp"
 
 Aggregator::Aggregator()
-  : _size(0)
+	: _size(0)
 {
-  this->_tmp = new char[maxSize];
+	this->_tmp = new char[maxSize];
 }
 
 Aggregator::~Aggregator()
 {
-  delete []_tmp;
+	delete []_tmp;
 }
 
 void	Aggregator::integrity(int sizeRead, char *buffer, APacket *packet)
 {
-  int	packLen = 0;
-  int	count = 0;
-  unsigned int check = 0;
+	int	packLen = 0;
+	int	count = 0;
+	unsigned int check = 0;
 
-  if (this->_size > 0)
-    {
-      if ((this->_size + sizeRead) > maxSize)
-	this->_size = 0;
-      memcpy(this->_tmp, this->_msg, this->_size);
-      memcpy(this->_tmp + this->_size, buffer, sizeRead);
-      sizeRead += this->_size;
-    }
-  else
-    memcpy(this->_tmp, buffer, sizeRead);
-  if (packet->unpack(this->_tmp, sizeRead) == false)
-    {
-      packLen = packet->getLength();
-      while (sizeRead >= packLen)
+	if (this->_size > 0)
 	{
-	  if (packet->unpack(this->_tmp + count, packLen) == true)
-	    {
-	      this->_buffer.push(std::pair<char *, size_t>(this->_tmp + count, packLen));
-	      sizeRead -= packLen;
-	      count += packLen;
-	    }
-	  packLen = packet->getLength();
-	  check++;
-	  if (check >= maxSize)
-	    sizeRead = 0;
+		if ((this->_size + sizeRead) > maxSize)
+			this->_size = 0;
+		memcpy(this->_tmp, this->_msg, this->_size);
+		memcpy(this->_tmp + this->_size, buffer, sizeRead);
+		sizeRead += this->_size;
 	}
-      if (sizeRead > 0 && sizeRead < packLen)
+	else
+		memcpy(this->_tmp, buffer, sizeRead);
+	if (packet->unpack(this->_tmp, sizeRead) == false)
 	{
-	  memcpy(this->_msg, this->_tmp + count, sizeRead);
-	  this->_size = sizeRead;
-	  return ;
+		packLen = packet->getLength();
+		while (sizeRead >= packLen)
+		{
+			if (packet->unpack(this->_tmp + count, packLen) == true)
+			{
+				this->_buffer.push(std::pair<char *, size_t>(this->_tmp + count, packLen));
+				sizeRead -= packLen;
+				count += packLen;
+			}
+			packLen = packet->getLength();
+			check++;
+			if (check >= maxSize)
+				sizeRead = 0;
+		}
+		if (sizeRead > 0 && sizeRead < packLen)
+		{
+			memcpy(this->_msg, this->_tmp + count, sizeRead);
+			this->_size = sizeRead;
+			return ;
+		}
+		else
+		{
+			this->_size = 0;
+			return ;
+		}
 	}
-      else
-	{
-	  this->_size = 0;
-	  return ;
-	}
-    }
-  this->_buffer.push(std::pair<char *, size_t>(buffer, sizeRead));
+	this->_buffer.push(std::pair<char *, size_t>(buffer, sizeRead));
 }
 
 std::pair<char *, size_t>  Aggregator::getMsgFront() const
 {
-  return this->_buffer.front();
+	return this->_buffer.front();
 }
 
 std::pair<char *, size_t> Aggregator::getMsgBack() const
 {
-  return this->_buffer.back();
+	return this->_buffer.back();
 }
 
 void	Aggregator::popMsg()
 {
-  this->_buffer.pop();
+	this->_buffer.pop();
 }
 
 bool	Aggregator::isEmpty()
 {
-  return _buffer.empty();
+	return _buffer.empty();
 }
--- Server/ListenerTCP.cpp
+++ Server/ListenerTCP.cpp
@@ -23,9 +23,9 @@
 #include "BinaryStream.hpp"
 
 ListenerTCP::ListenerTCP(unsigned short port)
-  : _port(port), _maxClient(99999), _log("ListenerTCP")
+	: _port(port), _maxClient(99999), _log("ListenerTCP")
 {
-  _log.addChannel(new Logging::ChannelConsole);
+	_log.addChannel(new Logging::ChannelConsole);
 }
 
 ListenerTCP::~ListenerTCP()
@@ -34,89 +34,88 @@ ListenerTCP::~ListenerTCP()
 
 bool	ListenerTCP::start()
 {
-  Network::Server	server(_port);
+	Network::Server	server(_port);
 
-  Logging::Message msg("Start TCP listening on port : " + Ultra::Value(_port).to<unsigned short, std::string>()
-		       + "...", "LTCP", Logging::Message::NOTICE);
-  this->_log << msg;
-  if (!server.run(_maxClient, ISocket::TCP))
-    {
-      Logging::Message msg("Failed to start", "LTCP", Logging::Message::_ERROR);
-      this->_log << msg;
-      return false;
-    }
+	Logging::Message msg("Start TCP listening on port : " + Ultra::Value(_port).to<unsigned short, std::string>()
+		+ "...", "LTCP", Logging::Message::NOTICE);
+	this->_log << msg;
+	if (!server.run(_maxClient, ISocket::TCP))
+	{
+		Logging::Message msg("Failed to start", "LTCP", Logging::Message::_ERROR);
+		this->_log << msg;
+		return false;
+	}
 
-  while (1)
-    {
-      Ultra::IThread *cthread = 0;
+	while (1)
+	{
+		Ultra::IThread *cthread = 0;
 
-      Network::Client	*client = server.acceptConnection();
+		Network::Client	*client = server.acceptConnection();
 
-      Logging::Message msg("Incoming connection from " + client->getHost(), "LTCP", Logging::Message::DEBUG);
-      this->_log << msg;
+		Logging::Message msg("Incoming connection from " + client->getHost(), "LTCP", Logging::Message::DEBUG);
+		this->_log << msg;
 
-      #ifdef _WIN32
-      cthread = new Ultra::WThread();
-      #else
-      cthread = new Ultra::LThread();
-      #endif
+#ifdef _WIN32
+		cthread = new Ultra::WThread();
+#else
+		cthread = new Ultra::LThread();
+#endif
 
-      cthread->create(&ListenerTCP::clientThread, client);
+		cthread->create(&ListenerTCP::clientThread, client);
 
-     }
-  return true;
+	}
+	return true;
 }
 
 void *ListenerTCP::run(void *listener)
 {
-  ListenerTCP *ltcp = reinterpret_cast<ListenerTCP *>(listener);
+	ListenerTCP *ltcp = reinterpret_cast<ListenerTCP *>(listener);
 
-  if (ltcp)
-    ltcp->start();
-  return (0);
+	if (ltcp)
+		ltcp->start();
+	return (0);
 }
 
 void *ListenerTCP::clientThread(void *param)
 {
-  int ret;
-  int sock;
-  char buffer[1024];
-  Network::Client *client;
-  Aggregator	agg;
+	int ret;
+	int sock;
+	char buffer[1024];
+	Network::Client *client;
+	Aggregator	agg;
 
-  client = reinterpret_cast<Network::Client *>(param);
-  Logging::Logger log("Thread:" + client->getHost());
-  log.addChannel(new Logging::ChannelConsole);
-  sock = client->getSocket();
-  if (client)
-    {
-      APacket *packet = new TCPPacket();
-      while (true)
+	client = reinterpret_cast<Network::Client *>(param);
+	Logging::Logger log("Thread:" + client->getHost());
+	log.addChannel(new Logging::ChannelConsole);
+	sock = client->getSocket();
+	if (client)
 	{
-	  if ((ret = client->receive(buffer, 1024)) <= 0)
-	    {
-	      Account *acc = Core::acc_manager->getAccount(sock);
-	      if (acc)
+		APacket *packet = new TCPPacket();
+		while (true)
 		{
-		  acc->setState(Account::OFFLINE);
-		  Core::acc_manager->delID(client->getSocket(ISocket::TCP));
+			if ((ret = client->receive(buffer, 1024)) <= 0)
+			{
+				Account *acc = Core::acc_manager->getAccount(sock);
+				if (acc)
+				{
+					acc->setState(Account::OFFLINE);
+					Core::acc_manager->delID(client->getSocket(ISocket::TCP));
+				}
+				client->disconnect();
+				Logging::Message l("Client disconnected " + client->getHost(), "LTCP", Logging::Message::DEBUG);
+				log << l;
+				return (0);
+			}
+			agg.integrity(ret, buffer, packet);
+			while (!agg.isEmpty())
+			{
+				packet->unpack(agg.getMsgFront().first, agg.getMsgFront().second);
+				agg.popMsg();
+				if (packet->getService() > ServiceManager::DISPATCH && packet->getService() < ServiceManager::COUNT)
+					Core::srv_manager->notifyService(packet->getService(),
+					new InternalMessage(packet, client->getSocket()));
+			}
 		}
-	      client->disconnect();
-	      Logging::Message l("Client disconnected " + client->getHost(), "LTCP", Logging::Message::DEBUG);
-	      log << l;
-	      return (0);
-	    }
-	  agg.integrity(ret, buffer, packet);
-	  while (!agg.isEmpty())
-	    {
-	      packet->unpack(agg.getMsgFront().first, agg.getMsgFront().second);
-	      agg.popMsg();
-
-	      if (packet->getService() > ServiceManager::DISPATCH && packet->getService() < ServiceManager::COUNT)
-		Core::srv_manager->notifyService(packet->getService(),
-						 new InternalMessage(packet, client->getSocket()));
-	    }
 	}
-    }
-  return (0);
+	return (0);
 }
